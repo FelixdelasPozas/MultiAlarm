fc2d5ce36faeef82c8f8484e9f0ceabfa819a6e6
Félix de las Pozas Álvarez <felixdelaspozas@gmail.com>
2025-12-19 21:26:52 +0100

    Fix: added missing dll in the installer script. Bump to version 1.4.1

--- AboutDialog.cpp
+++ AboutDialog.cpp
@@ -27,7 +27,7 @@
 #include <QDateTime>
 #include <QtGlobal>
 
-const QString AboutDialog::VERSION = QString("version 1.4.0");
+const QString AboutDialog::VERSION = QString("version 1.4.1");
 
 //-----------------------------------------------------------------
 AboutDialog::AboutDialog(QWidget *parent, Qt::WindowFlags flags)
